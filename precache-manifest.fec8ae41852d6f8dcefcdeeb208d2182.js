cc3f9b5be0fbfacb2dbdc8ed61813191847e571f
Liam Corrigan <liam.corr@gmail.com>
2021-08-31 00:41:48 -0400

    Updates

---
+++ precache-manifest.fec8ae41852d6f8dcefcdeeb208d2182.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53ad9e04b9f17d95744",
+    "url": "/fancy-fox-landing-page/css/app.4d279115.css"
+  },
+  {
+    "revision": "992ec3465605736013c9b5086a821fce",
+    "url": "/fancy-fox-landing-page/img/logo-fox-xlarge.png"
+  },
+  {
+    "revision": "3541ffdca0b31046c981dbf1666b155a",
+    "url": "/fancy-fox-landing-page/img/logo-fox.png"
+  },
+  {
+    "revision": "927c40c62f716f9ac8a736165002025f",
+    "url": "/fancy-fox-landing-page/img/space.jpg"
+  },
+  {
+    "revision": "dec5c1d8af43766fcca60e35a28123b2",
+    "url": "/fancy-fox-landing-page/index.html"
+  },
+  {
+    "revision": "15096d129f9fd4bf7395",
+    "url": "/fancy-fox-landing-page/js/about.cee2355b.js"
+  },
+  {
+    "revision": "c53ad9e04b9f17d95744",
+    "url": "/fancy-fox-landing-page/js/app.e6bd40bf.js"
+  },
+  {
+    "revision": "e6f5b70a00eac0978b4c",
+    "url": "/fancy-fox-landing-page/js/chunk-vendors.ee23c5bb.js"
+  },
+  {
+    "revision": "c017ddc1419f5e27f1b9a298f3fb88ad",
+    "url": "/fancy-fox-landing-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fancy-fox-landing-page/robots.txt"
+  },
+  {
+    "revision": "f21f45b2c83bca70d4db08e467003f7b",
+    "url": "/fancy-fox-landing-page/style.css"
+  }
+]);
