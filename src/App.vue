720dc760ff144af67f945322edb382b4b4424d0e
Liam Corrigan <liam.corr@gmail.com>
2021-08-31 00:29:29 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="scene">
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+      <div class="fox">
+        <h1 class="centraltext2" style="font-family: Georgia, 'Times New Roman', Times, serif; color: wheat;">Fancy</h1>
+      </div>
+      <div class="img-container">
+        <img class="fade-in-image " src="img/logo-fox.png" ></img>
+      </div>
+      <div class="fox">
+        <h1 class="centraltext" style="font-family: Georgia, 'Times New Roman', Times, serif; color: wheat;">Fancy</h1>
+      </div>
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+    </div>
+
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="scene">
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+      <div class="fox">
+        <h1 class="centraltext2" style="font-family: Georgia, 'Times New Roman', Times, serif; color: wheat;">Fancy</h1>
+      </div>
+      <div class="img-container">
+        <img class="fade-in-image " src="img/logo-fox.png" ></img>
+      </div>
+      <div class="fox">
+        <h1 class="centraltext" style="font-family: Georgia, 'Times New Roman', Times, serif; color: wheat;">Fancy</h1>
+      </div>
+      <div class="wrap">
+        <div class="wall wall-right"></div>
+        <div class="wall wall-left"></div>
+        <div class="wall wall-top"></div>
+        <div class="wall wall-bottom"></div>
+        <div class="wall wall-back"></div>
+      </div>
+    </div>
+
+
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
